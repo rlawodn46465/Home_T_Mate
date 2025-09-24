b697a1bdf654fab800035bedbcf7d594d03b1201
rlawodn46465 <rlawodn46465@naver.com>
2025-09-24 17:08:46 +0900

    [FE Design] 헤더 UI 제작

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,9 +1,33 @@
-import "./Header.css"
+import "./Header.css";
+import list_icon from "../assets/images/list_icon.svg";
+import training_icon from "../assets/images/training_icon.svg";
+import routine_icon from "../assets/images/routine_icon.svg";
+import user_icon from "../assets/images/user_icon.svg";
 
-function Header(){
-  return(
-    <div className="header-container">머리</div>
-  )
+function Header() {
+  return (
+    <div className="header-container">
+      <h1>홈트메이트</h1>
+      <ul className="menu-container">
+        <li className="menu-item">
+          <img src={list_icon} />
+          <p>게시판</p>
+        </li>
+        <li className="menu-item">
+          <img src={training_icon} />
+          <p>운동</p>
+        </li>
+        <li className="menu-item">
+          <img src={routine_icon} />
+          <p>루틴</p>
+        </li>
+        <li className="menu-item">
+          <img src={user_icon} />
+          <p>마이페이지</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
