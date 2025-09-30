51202fe70a2d94b104d3d7042698f1daa4bf4541
rlawodn46465 <rlawodn46465@naver.com>
2025-09-30 19:59:12 +0900

    [FE Design] 메인 컨텐츠 레이아웃 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css";
 import CommunityPage from "./pages/Community/CommunityPage";
 import Header from "./pages/Header";
-import Login from "./pages/Home/Login";
+import Login from "./pages/Login/LoginPage";
 
 const App = () => {
   return (
     <div className="main-container">
       <Header />
       <div className="main-content">
-        <CommunityPage />
-        <Login />
+        <div className="left-content">
+          <CommunityPage />
+        </div>
+        <div className="right-content">
+          <Login />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
