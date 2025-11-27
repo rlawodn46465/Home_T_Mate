3253ef4ea043924c1160857acdc22af384be94de
rlawodn46465 <rlawodn46465@naver.com>
2025-11-27 21:43:14 +0900

    [FE MISC] 뒤로가기 버튼 기능 추가

--- src/components/ui/ExerciseList/ExerciseForm/ExerciseFormContainer.jsx
+++ src/components/ui/ExerciseList/ExerciseForm/ExerciseFormContainer.jsx
@@ -1,13 +1,22 @@
 import ExerciseInfoSection from "./ExerciseInfoSection";
 
 import "./ExerciseFormContainer.css";
+import { useNavigate } from "react-router-dom";
 
 const ExerciseFormContainer = ({ exerciseId }) => {
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate("/?panel=record");
+  };
+
   return (
     <div className="exercise-form-container">
       <div className="exercise-form-container__header">
         <h2>운동 기록 추가</h2>
-        <button className="form-cancel-button">취소</button>
+        <button className="form-cancel-button" onClick={handleCancel}>
+          취소
+        </button>
       </div>
       <ExerciseInfoSection />
     </div>
