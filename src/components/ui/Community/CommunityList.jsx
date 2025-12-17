469153d12bbbd61eaf511ccf8effd7c18a45d6b1
rlawodn46465 <rlawodn46465@naver.com>
2025-12-18 03:45:41 +0900

    [FE MISC] 더미 데이터 제거

--- src/components/ui/Community/CommunityList.jsx
+++ src/components/ui/Community/CommunityList.jsx
@@ -15,58 +15,6 @@ const CommunityList = ({
   onPageChange,
   onItemClick,
 }) => {
-  // const listItem = [
-  //   {
-  //     id: 1,
-  //     tag: 2,
-  //     title: "맨몸 운동 3분할 루틴",
-  //     user: "나님",
-  //     commentCount: 2,
-  //     goal: false,
-  //     like: 2,
-  //     date: "2025-09-10 14:32",
-  //   },
-  //   {
-  //     id: 2,
-  //     tag: 3,
-  //     title: "맨몸 운동 3분할 루틴",
-  //     user: "나님",
-  //     commentCount: 2,
-  //     goal: false,
-  //     like: 2,
-  //     date: "2025-09-10 14:32",
-  //   },
-  //   {
-  //     id: 3,
-  //     tag: 2,
-  //     title: "맨몸 운동 3분할 루틴",
-  //     user: "나님",
-  //     commentCount: 2,
-  //     goal: false,
-  //     like: 2,
-  //     date: "2025-09-10 14:32",
-  //   },
-  //   {
-  //     id: 4,
-  //     tag: 3,
-  //     title: "맨몸 운동 3분할 루틴",
-  //     user: "나님",
-  //     commentCount: 2,
-  //     goal: true,
-  //     like: 2,
-  //     date: "2025-09-10 14:32",
-  //   },
-  //   {
-  //     id: 5,
-  //     tag: 2,
-  //     title: "맨몸 운동 3분할 루틴",
-  //     user: "나님",
-  //     commentCount: 2,
-  //     goal: false,
-  //     like: 2,
-  //     date: "2025-09-10 14:32",
-  //   },
-  // ];
 
   if (isLoading) {
     return <div className="loading-indicator">게시글 로딩 중...</div>;
